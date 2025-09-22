4407c1de8d2b14bfbe254ec9ddabf8ec06aff7de
buyyakhoo <pollapat13799@gmail.com>
2025-09-23 00:33:34 +0700

    add sys melody & chordbg

--- future_space.rb
+++ future_space.rb
@@ -1,22 +1,35 @@
 use_bpm 120
 
-amp_beat = 0.8
+amp_beat = 0.7
+amp_heart_beat = 1.3
 amp_snare = 0.4
 amp_hat = 0.5
-amp_bg_synth = 0.6
+amp_bg_synth = 0.3
 amp_chord1st_synth = 0.5
 amp_chord2nd_synth = 0.3
+
+amp_system_melody = 0.5
+amp_system_chordbg_synth = 0.1
 
 live_loop :met do
   sleep 1
 end
 
+live_loop :heart_beat, sync: :met do
+  stop
+  sample :bd_fat, amp: amp_heart_beat
+  sleep 0.25
+  sample :bd_fat, amp: amp_heart_beat*0.5
+  sleep 0.75
+end
+
 live_loop :beat, sync: :met do
-  sample :bd_klub, amp: amp_beat
+  sample :bd_fat, amp: amp_beat
   sleep 1
 end
 
 live_loop :snare, sync: :met do
+  stop
   sleep 2
   with_fx :echo do
     with_fx :bitcrusher, sample_rate: 8000 do
@@ -27,6 +40,7 @@ live_loop :snare, sync: :met do
 end
 
 live_loop :hat, sync: :met do
+  stop
   hit = "-xx-x-xx-x-xx-x-".ring
   16.times do
     with_fx :bitcrusher, sample_rate: 4000 do
@@ -40,6 +54,7 @@ end
 live_loop :bg_synth, sync: :met do
   use_synth :prophet
   note_list = [:A1, :D1].ring
+  #note_list = [:Cs5, :Fs4].ring
   
   with_fx :lpf, cutoff: 60 do
     play note_list.tick, sustain: 8, release: 0, amp: amp_bg_synth
@@ -48,6 +63,7 @@ live_loop :bg_synth, sync: :met do
 end
 
 live_loop :chord1st_synth, sync: :met do
+  stop
   use_synth :supersaw
   note_list = [:A3, :D4].ring
   with_fx :reverb, room: 0.9, mix: 0.7 do
@@ -65,6 +81,7 @@ live_loop :chord1st_synth, sync: :met do
 end
 
 live_loop :chord2nd_synth, sync: :met do
+  stop
   use_synth :supersaw
   note_list = [:A4, :D5].ring
   with_fx :reverb, room: 0.9, mix: 0.7 do
@@ -74,6 +91,33 @@ live_loop :chord2nd_synth, sync: :met do
   end
   sleep 8
 end
+
+live_loop :synth_system_melody, sync: :met do
+  stop
+  use_synth :square
+  note_list = [:A4, :D4, :E4, :A5, :Fs5, :Gs5, :E5, :Cs5].ring
+  
+  8.times do
+    with_fx :reverb, room: 0.7, mix: 0.8 do
+      with_fx :lpf, cutoff: 65 do
+        play note_list.choose, sustain: 0.1, release: 0.1, amp: amp_system_melody
+      end
+    end
+    sleep 0.5
+  end
+end
+
+live_loop :system_chordbg_synth, sync: :met do
+  stop
+  use_synth :supersaw
+  with_fx :reverb, room: 0.9, mix: 1 do
+    with_fx :ixi_techno, phase: 16, cutoff_min: 60, cutoff_max: 100 do
+      play_chord chord(:A4, :major7), sustain: 15.5, release: 1, amp: amp_system_chordbg_synth
+    end
+  end
+  sleep 16
+end
+
 
 # A C# E G# A G# E A
 # B D F# A G# - A B
